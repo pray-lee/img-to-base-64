e63df4e872afa69f6a965b024efedff7b3b21cae
pray-lee <praycis@gmail.com>
2018-08-20 11:39:02 +0800

    base-64

---
+++ dist/main.js
@@ -0,0 +1,101 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = "./src/index.js");
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+eval("\n\ndocument.addEventListener('readystatechange', function (state) {\n  if (document.readyState === 'complete') {\n    var fileDom = document.getElementById('file') || null;\n    var file = null;\n\n    fileDom.onchange = function (e) {\n      file = this.files[0];\n    };\n\n    var button = document.getElementsByClassName('btn-success')[0];\n    button.addEventListener('click', function () {\n      var next = checkImg(file);\n      console.log(next);\n      if (next) codeImg(file);else return false;\n    }, false);\n  }\n}, false);\n\nfunction codeImg(file) {\n  if (file) {\n    var reader = new FileReader();\n    reader.readAsDataURL(file);\n\n    reader.onload = function () {\n      var baseContent = document.getElementsByClassName('card-text')[0];\n      baseContent.innerHTML = String(this.result);\n    };\n  }\n}\n\nfunction checkImg(file) {\n  var ext = file.name.toLowerCase() || '';\n  var size = file.size;\n\n  if (ext != 'jpg' && ext != 'png' && ext != 'jpeg' && ext != 'gif') {\n    alert('只能输入jpg，png，jpeg，gif格式');\n    return false;\n  }\n\n  if (size > 8 * 1024) {\n    alert('图片大小不超过8KB');\n  }\n}\n\n//# sourceURL=webpack:///./src/index.js?");
+
+/***/ })
+
+/******/ });
---
+++ src/index.js
@@ -0,0 +1,42 @@
+document.addEventListener('readystatechange', (state)=>{
+	if(document.readyState === 'complete'){
+		let fileDom = document.getElementById('file') || null
+		let file = null
+		fileDom.onchange = function(e) {
+			file = this.files[0]
+		}
+		let button = document.getElementsByClassName('btn-success')[0]
+		button.addEventListener('click', ()=>{
+			let next = checkImg(file)
+			console.log(next)
+			if(next)
+				codeImg(file)
+			else
+				return false
+		}, false)
+	}
+}, false)
+
+function codeImg(file) {
+	if(file){
+		let reader = new FileReader();
+		reader.readAsDataURL(file);
+		reader.onload = function(){
+			const baseContent = document.getElementsByClassName('card-text')[0]
+			baseContent.innerHTML = String(this.result)	
+		}
+
+	}
+}
+
+function checkImg(file) {
+	let ext = file.name.toLowerCase() || ''
+	let size = file.size
+	if(ext != 'jpg' && ext != 'png' && ext != 'jpeg' && ext != 'gif'){
+		alert('只能输入jpg，png，jpeg，gif格式')
+		return false
+	}
+	if(size>(8*1024)){
+		alert('图片大小不超过8KB')
+	}
+}
